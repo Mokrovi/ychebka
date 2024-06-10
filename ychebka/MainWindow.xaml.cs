83bb7222a7f38a3b75fdb7aac1e02f4e4ccbadaf
Mokrovi <xxantonxx21@mail.ru>
2024-06-11 01:49:46 +0300

    как будто всё

--- ychebka/MainWindow.xaml.cs
+++ ychebka/MainWindow.xaml.cs
@@ -28,31 +28,37 @@ namespace ychebka
         private void OpenClient(object sender, RoutedEventArgs e)
         {
             Main.Content = new PageClient();
+            wind.Title = "Окно клиент";
         }
 
         private void OpenRieltor(object sender, RoutedEventArgs e)
         {
             Main.Content = new PageRieltor();
+            wind.Title = "Окно риэлтора";
         }
 
         private void OpenEstate(object sender, RoutedEventArgs e)
         {
             Main.Content = new PageEstate();
+            wind.Title = "Окно недвижемости";
         }
 
         private void OpenDeal(object sender, RoutedEventArgs e)
         {
             Main.Content = new PageDeal();
+            wind.Title = "Окно сделки";
         }
 
         private void OpenSuggestion(object sender, RoutedEventArgs e)
         {
             Main.Content = new PageSuggestion();
+            wind.Title = "Окно Предложений";
         }
 
         private void OpenNeed(object sender, RoutedEventArgs e)
         {
             Main.Content = new PageNeed();
+            wind.Title = "Окно потребностей";
         }
     }
 }
